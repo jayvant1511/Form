fce2fd647689b8d30ecdfbe4431c4e4add8bc6e4
jayvant1511 <padsala.kiritbhai@digital.datamatics.com>
2021-04-13 21:04:39 +0530

    remove prev coed

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ContactUs from "./Pages/ContactUs";
-import Reports from "./Pages/Reports";
-import Kyc from "./Pages/Home";
-
-import  Sidebar  from './SideBar.js';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path='/kyc' component={Kyc} />
-            <Route path='/reports'   component={Reports} />
-            <Route path='/contactUs'  component={ContactUs} />
-          </Switch>
-        </Router>  
+        Hello
       </div>
     );
   }
